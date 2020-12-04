5ebbe8392643c831add76dcb08b04e99eab3478f
Myasnik <khachkalyan.myasnik@gmail.com>
2020-12-04 16:46:10 +0400

    [change] gerb to use sprite

--- src/components/Gerb/Gerb.tsx
+++ src/components/Gerb/Gerb.tsx
@@ -1,16 +1,13 @@
 import React, { FC } from 'react';
 import styles from './Gerb.module.scss';
-import standingStateImg from '../../assets/images/other/gerb.svg';
-import destroyedStateImg from '../../assets/images/other/gerb_destroyed.svg';
 import { useGridObjectState } from '../Grid/Grid.helpers';
 import { GerbState } from '../../models/GerbObject';
 
 const Gerb: FC = () => {
   const [state, inlineStyles] = useGridObjectState<GerbState>();
-  const gerbImg = state.isDestroyed ? destroyedStateImg : standingStateImg;
-
+  const classes = [styles.Gerb, state.isDestroyed ? styles.Destroyed : styles.Standing].join(' ');
   return (
-    <img className={styles.Gerb} style={inlineStyles} src={gerbImg} alt="" />
+    <div className={classes} style={inlineStyles} />
   );
 };
 
