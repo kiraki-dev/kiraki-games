d065343bba294eb1dd275e41b874c493d8e9145b
Myasnik <khachkalyan.myasnik@gmail.com>
2020-12-13 18:53:35 +0400

    [change] level to use sprite

--- src/components/Level/Level.tsx
+++ src/components/Level/Level.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from 'react';
 import styles from './Level.module.scss';
-import lvlFlagImg from '../../assets/images/other/level_flag.png';
 
 const Level: FC<Readonly<{ lvl: number; }>> = (props: Readonly<{ lvl: number; }>) => (
   <div className={styles.Level}>
-    <img src={lvlFlagImg} alt="" />
+    <div className={styles.Img} />
     <div>
       <span>{props.lvl}</span>
     </div>
