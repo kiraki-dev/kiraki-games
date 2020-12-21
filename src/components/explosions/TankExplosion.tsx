c5f6d6b9fc6425efb7d77cb37930e1f73cf51bd8
Myasnik <khachkalyan.myasnik@gmail.com>
2020-12-21 12:55:40 +0400

    [change] tank explosion to use sprite

--- src/components/explosions/TankExplosion.tsx
+++ src/components/explosions/TankExplosion.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from 'react';
 import classes from './TankExplosion.module.scss';
+import { useGridObjectState } from '../Grid/Grid.helpers';
 
-const TankExplosion: FC = () => <div className={classes.TankExplosion} />;
+const TankExplosion: FC = () => {
+  const [, inlineStyles] = useGridObjectState<undefined>();
+  return <div style={inlineStyles} className={classes.TankExplosion} />;
+};
 
 export default TankExplosion;
